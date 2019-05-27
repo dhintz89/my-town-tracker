a9ffb6338a3cda36d05d3da6bf9b53ea7f07fd32
Dan Hintz <dhintz@live.com>
2019-05-27 03:36:24 +0000

    build out user_create functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,26 @@
 class UsersController < ApplicationController
 
-  get '/users/signup' do
-    erb :"users/create_user"
-  end
+    get '/users/signup' do
+      erb :"users/create_user"
+    end
   
-  get '/users/login' do
-    erb :"users/login"
+    post '/users' do
+      @user = User.create(params[:user])
+      if @user.username != "" && @user.save
+        session[:user_id] = @user.id
+        redirect "/users/#{@user.id}"
+      else
+        redirect "/users/signup"
+      end
+    end
+    
+    get '/users/login' do
+      erb :"users/login"
+    end
+    
+    get "/users/:id" do
+      @user = User.find(params[:id])
+      erb :"users/show"
+    end
+    
   end
-  
-  post '/users/signup' do
-    @user = User.create(params[:user])
-    redirect "/users/#{@user.id}"
-  end
-  
-  get "/users/:id" do
-    @user = User.find(params[:id])
-    erb :"users/show"
-  end
-  
-end
