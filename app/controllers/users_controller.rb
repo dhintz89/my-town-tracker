f429008b392092f35a9280ab2582019ed05e141d
Dan Hintz <dhintz@live.com>
2019-05-27 02:57:21 +0000

    added basic create_user function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,21 @@
 class UsersController < ApplicationController
-  
+
   get '/users/signup' do
     erb :"users/create_user"
   end
   
   get '/users/login' do
     erb :"users/login"
+  end
+  
+  post '/users/signup' do
+    @user = User.create(params[:user])
+    redirect "/users/#{@user.id}"
+  end
+  
+  get "/users/:id" do
+    @user = User.find(params[:id])
+    erb :"users/show"
   end
   
 end
--- config/environment.rb
+++ config/environment.rb
@@ -3,7 +3,7 @@ Bundler.require
 
 ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
-  :database => "db/[db_name].sqlite"
+  :database => "db/town_tracker.sqlite"
 )
 
 require_all 'app'
