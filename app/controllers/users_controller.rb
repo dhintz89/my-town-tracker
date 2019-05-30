3a9f3dd4b67b4f8f7198b5bac8f1ef2d6473583e
Dan Hintz <dhintz@live.com>
2019-05-30 02:33:54 +0000

    add login function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,26 +1,36 @@
 class UsersController < ApplicationController
 
-    get '/users/signup' do
-      erb :"users/create_user"
+  get '/users/signup' do
+    erb :"users/create_user"
+  end
+
+  post '/signup' do
+    @user = User.create(params[:user])
+    if @user.username != "" && @user.save
+      session[:user_id] = @user.id
+      redirect "/users/#{@user.id}"
+    else
+      redirect "/signup"
     end
+  end
   
-    post '/users' do
-      @user = User.create(params[:user])
-      if @user.username != "" && @user.save
-        session[:user_id] = @user.id
-        redirect "/users/#{@user.id}"
-      else
-        redirect "/users/signup"
-      end
+  get '/users/login' do
+    erb :"users/login"
+  end
+  
+  post '/login' do
+    @user = User.find_by(username: params[:user][:username])
+    if @user && @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id
+      redirect "/users/#{@user.id}"
+    else
+      redirect "/login"
     end
-    
-    get '/users/login' do
-      erb :"users/login"
-    end
-    
-    get "/users/:id" do
-      @user = User.find(params[:id])
-      erb :"users/show"
-    end
+  end
+  
+  get "/users/:id" do
+    @user = User.find(params[:id])
+    erb :"users/show"
+  end
     
   end
