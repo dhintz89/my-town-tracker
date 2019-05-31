e0793351eaa602e2e3e19fb7c1a16130642c629a
Dan Hintz <dhintz@live.com>
2019-05-31 20:05:52 +0000

    add ActiveRecord Validation to Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
   has_many :places
   has_many :recommendations, through: :places
   has_many :categories, through: :places
+  validates :name,  presence: true
+  validates :username, presence: true, uniqueness: {case_sensitive: false}
   has_secure_password
   
 end
