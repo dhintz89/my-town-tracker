67267b27d13b7b6391fb7d4505b2456e2d8c8977
Dan Hintz <dhintz@live.com>
2019-06-01 19:29:49 +0000

    complete new_place form

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -6,13 +6,20 @@ class PlacesController < ApplicationController
   end
   
   get '/places/new' do
+    @recommendations = Recommendation.all
+    @categories = Category.all
     erb :"places/new"
   end
   
   post '/places' do
-    @place = Place.new(name: params[:place][:name])
-    @place.category = Category.find_or_create_by(name: params[:place][:category])
-    # params[:place][:recommendation].each {|rec_id| @place.recommendations << Recommendation.find_by_id(rec_id)}
+    @place = Place.create(params[:place])
+    if !params[:category][:name].empty?
+      @place.category = Category.find_or_create_by(name: params[:category][:name])
+      @place.save
+    end
+    if !params[:recommendation][:name].empty?
+      @place.recommendations << Recommendation.find_or_create_by(name: params[:recommendation][:name])
+    end
     redirect "/places/#{@place.id}"
   end
   
