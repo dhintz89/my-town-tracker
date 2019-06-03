b39d41e3944420c55bf716fc5f4b0f4dc520312b
Dan Hintz <dhintz@live.com>
2019-06-03 23:31:50 +0000

    some user controller actions pushed to app controller, startpassword change funtion

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,4 +15,51 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
   
+  get '/signup' do
+    erb :"users/create_user"
+  end
+
+  post '/signup' do
+    @user = User.new(params[:user])
+    if @user.valid? && @user.save
+      session[:user_id] = @user.id
+      flash[:message] = "User successfully created"
+      redirect "/users/#{@user.id}"
+    else
+      flash[:message] = "Problem creating new user. Please try again."
+      redirect "/signup"
+    end
+  end
+  
+  get '/login' do
+    erb :"users/login"
+  end
+  
+  post '/login' do
+    @user = User.find_by(username: params[:user][:username])
+    if @user && @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id
+      redirect "/users/#{@user.id}"
+    else
+      flash[:message] = "Incorrect username or password. Please try again."
+      redirect "/login"
+    end
+  end
+
+  get '/logout' do
+    session.clear
+    flash[:message] = "Successfully logged out"
+    redirect '/'
+  end
+  
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
+    
+    def current_user
+      User.find(session[:user_id])
+    end
+  end
+  
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,45 +1,29 @@
 class UsersController < ApplicationController
 
-  get '/signup' do
-    erb :"users/create_user"
-  end
-
-  post '/signup' do
-    @user = User.new(params[:user])
-    if @user.valid? && @user.save
-      session[:user_id] = @user.id
-      flash[:message] = "User successfully created"
-      redirect "/users/#{@user.id}"
-    else
-      flash[:message] = "Problem creating new user. Please try again."
-      redirect "/signup"
-    end
-  end
-  
-  get '/login' do
-    erb :"users/login"
-  end
-  
-  post '/login' do
-    @user = User.find_by(username: params[:user][:username])
-    if @user && @user.authenticate(params[:user][:password])
-      session[:user_id] = @user.id
-      redirect "/users/#{@user.id}"
-    else
-      flash[:message] = "Incorrect username or password. Please try again."
-      redirect "/login"
-    end
-  end
-
-  get '/logout' do
-    session.clear
-    flash[:message] = "Successfully logged out"
-    redirect '/'
-  end
-  
   get "/users/:id" do
     @user = User.find(params[:id])
     erb :"users/show"
+  end
+  
+  get "/users/:id/change_password" do
+    @user = User.find(params[:id])
+    erb :"users/change_password"
+  end
+  
+  get "/users/:id/edit" do
+    
+  end
+  
+  patch "/users/:id" do
+    binding.pry
+    if params.keys.include?("new_password")
+      
+      if @user.id = current_user.id && @user.authenticate(params[:current_password]) && params[:new_password]==params[:confirm_password]
+        @user.password = params[:new_password]
+      else
+        redirect "/users/#{@user.id}/change_password"
+      end
+    end
   end
   
   helpers do
