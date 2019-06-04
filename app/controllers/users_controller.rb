56dec0a9e2efb7c07b5b86fd524e64054b485d87
Dan Hintz <dhintz@live.com>
2019-06-04 01:08:32 +0000

    add user editing functions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,17 +11,32 @@ class UsersController < ApplicationController
   end
   
   get "/users/:id/edit" do
-    
+    @user = User.find(params[:id])
+    erb :"users/edit"
   end
   
   patch "/users/:id" do
-    binding.pry
+    @user = User.find(params[:id])
     if params.keys.include?("new_password")
       
-      if @user.id = current_user.id && @user.authenticate(params[:current_password]) && params[:new_password]==params[:confirm_password]
-        @user.password = params[:new_password]
+      if @user.authenticate(params[:current_password]) && params[:new_password]==params[:confirm_password]
+        @user.update(password: params[:new_password])
+        @user.save
+        # password not actually changing
+        flash[:message] = "Password successfully changed"
+        redirect "/users/#{@user.id}"
       else
+        flash[:message] = "Password could not be changed. Please try again."
         redirect "/users/#{@user.id}/change_password"
+      end
+    else
+      if @user.authenticate(params[:current_password])
+        @user.update(params[:user])
+        flash[:message] = "Profile successfully updated."
+        redirect "/users/#{@user.id}"
+      else
+        flash[:message] = "Changes could not be saved, please check that your password was entered correctly."
+        redirect "/users/#{@user.id}/edit"
       end
     end
   end
